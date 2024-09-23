cce31eb867c5758c76de564e110a1e3846372146
noboruhi <noboruhi@gmail.com>
2024-09-23 19:43:26 +0900

    猫更新ボタンを追加。

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,20 @@ const IndexPage: NextPage = () => {
       setLoading(false); // ローディング状態を更新する
     });
   }, []);
+  // ボタンクリックで画像を読み込む処理
+  const handleClick = async () => {
+    setLoading(true); // 読み込み中フラグを立てる
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url); // 画像URLの状態を更新する
+    setLoading(false); //  読み込み中フラグ倒す
+  };
   // ローディングでなければ、画像を表示する
-  return <div>{loading || <img src={imageUrl} />}</div>;
+  return (
+    <div>
+      <button onClick={handleClick}>他のにゃんこも見る</button>
+      <div>{loading || <img src={imageUrl} />}</div>
+    </div>
+  );
 };
 export default IndexPage;
 type Image = {
